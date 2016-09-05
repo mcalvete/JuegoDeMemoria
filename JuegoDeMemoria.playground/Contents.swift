9c71bcbc55db209ecf2fc0502d5356b3a4933cd6
Martín Calvete <martin@martins-mac.local>
2016-09-05 19:30:26 +0200

    Código del juego de memoria

--- JuegoDeMemoria.playground/Contents.swift
+++ JuegoDeMemoria.playground/Contents.swift
@@ -1,5 +1,19 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+var numeros = 0...100
+for numero in numeros {
+    
+    if numero % 5 == 0 {
+        print("# \(numero) Bingo!!!")
+    }
+    if numero % 2 == 0 {
+        print("# \(numero) Par!!!")
+    }
+    if numero % 2 != 0 {
+        print("# \(numero) Impar!!!")
+    }
+    if numero >= 30 && numero <= 40 {
+        print("# \(numero) Viva Swift!!!")
+    }
+    
+}
